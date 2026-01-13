039a060f1cb0057525294d31a0d31295fce07d60
limr <robert.lim2001@gmail.com>
2026-01-13 14:21:26 +0100

    chore: ignore python cache files

--- app.py
+++
@@ -1,7 +0,0 @@
-# app.py — point d’entrée
-from shiny import App
-from pathlib import Path
-from ui import app_ui
-from server import make_server
-
-app = App(app_ui, make_server(Path(__file__).parent))
